6a16292f106cf62967f81206ea82fc9e77f63a38
timtoronto634 <tim225x@gmail.com>
2022-10-09 01:16:42 +0000

    Improve 272b

--- atcoder/272/b/main.cpp
+++ atcoder/272/b/main.cpp
@@ -2,41 +2,26 @@
 using namespace std;
 
 void solve() {
-    int people;
-    int butou;
-    cin >> people;
-    cin >> butou;
-    // create matrix of bool people x people
-    vector<vector<bool>> mat(people, vector<bool>(people));
-    // for each butou
-      int participants;
-    for (int b=0;b<butou; b++){
-      cin >> participants;
-      vector<int> part(participants, 0);
-      for (int ip=0;ip<participants;ip++){
-        int p;
-        cin >> p;
-        part[ip] = p-1;
-        // cout << part[ip] <<endl;
-      }
-      for (int ii=0;ii<participants;ii++) {
-        for (int jj=ii;jj<participants;jj++) {
-          mat[part[ii]][part[jj]] = true;
-          mat[part[jj]][part[ii]] = true;
-        }
-      }
+  int n,m;
+  int k, vis[105][105];
+  int a[105];
+
+  cin >>n>>m;
+
+  for (int i=1;i<=m;i++) {
+    cin >> k;
+    for (int j=1;j<=k;j++) cin >> a[j];
+    for (int j=1;j<=k;j++) for (int t=j+1;t<=k;t++){
+      vis[a[j]][a[t]] = vis[a[t]][a[j]]=1;
     }
-    for (int eachp=0;eachp<people;eachp++) {
-      for (int pp=0;pp<people;pp++) {
-        // cout << mat[eachp][pp] <<endl;
-        if (!(mat[eachp][pp])) {
-          cout << "No" <<endl;
-          return;
-        }
-      }
+  }
+  for (int i=1;i<=n;i++)
+  for (int j=i+1;j<=n;j++) {
+    if (vis[i][j] == 0) {
+      cout << "No"<<endl;return;
     }
-    // for each combination of people (double loop), make the corresponding cell true
-    // finally check through matix whether false exists.
+  }
+
     cout << "Yes" << endl;
 }
 
