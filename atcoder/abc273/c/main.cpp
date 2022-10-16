fbc0cb959407508af79264a8b7b69d4c7873cf94
timtoronto634 <tim225x@gmail.com>
2022-10-16 12:26:23 +0000

    Finish abc c

--- atcoder/abc273/c/main.cpp
+++ atcoder/abc273/c/main.cpp
@@ -14,21 +14,36 @@ int main() {
   }
   map<int, int> cnt;
   for (int i=0;i<n;i++) {
-    // cout << v[i]  << endl;
-    cnt.insert(make_pair(v[i], 1));
+    int found = cnt.count(v[i]);
+    if (found == 0) {
+      cnt.insert(make_pair(v[i], 1));
+    } else {
+      cnt[v[i]] = cnt[v[i]] + 1;
+      // cout << cnt[v[i]] << " here" << endl;
+    }
     // cout << cnt[v[i]] << endl;
   }
   int rest = cnt.size();
   vector<int> out(n+1, 0);
+  map<int, int> num2numlarger;
   int idx = 1;
   for (auto x : cnt) {
-    // cout << x.first << endl;
     rest--;
-    out[idx] = rest;
-    
+    num2numlarger.insert(make_pair(x.first, rest));
   }
-  for (int i=1;i<=n+1;i++) {
-    cout << out[i] << endl;
+  map<int, int> numkind2numnums;
+  for (auto y: num2numlarger) {
+    numkind2numnums.insert(make_pair(y.second, cnt[y.first]));
+    // cout << y.second << ":" << y.first << ":" << cnt[y.first] << endl;
+  }
+
+  for (int i=0;i<n;i++) {
+    int kkk = numkind2numnums.count(i);
+    if (kkk == 0) {
+      cout << 0 << endl;
+    } else {
+      cout << numkind2numnums[i] << endl;
+    }
   }
 
   return 0;
