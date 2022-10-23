68e82e332efb0f2bc25a7dba463f85954a9a8dbb
timtoronto634 <tim225x@gmail.com>
2022-10-23 17:13:55 +0000

    abc274 d trial and errors

--- atcoder/abc274/d/main.cpp
+++ atcoder/abc274/d/main.cpp
@@ -2,56 +2,36 @@
 using namespace std;
 using ll = long long int;
 
+bool findArrWithTargetSum(vector<int>& av, int curSum, int targetSum) {
+  if (curSum < targetSum) return false;
+  if (curSum == targetSum) return true;
+  int idx = 0;
+  vector<int> idxVal;
+  // vector<int> sidx(0);
+  // vector<int> sval(0);
 
-bool findArrWith2TargetSum(vector<int> v, int targetSumA, int targetSumB, int targetSum) {
-  sort(v.begin(), v.end());
-  int thisTarget = min(targetSumA, targetSumB);
-  stack<pair<int, int>> idxVal;
-  int curSum = 0, idx = 0;
-  while (idx < v.size()) {
-    while (curSum < targetSumA) {
-      curSum += v[idx];
-      idxVal.push(make_pair(idx, v[idx]));
-      idx++;
-    }
-    if (curSum == thisTarget) return true;
-
-    if(idxVal.empty()) break;
-    auto p = idxVal.top();
-    curSum -= p.second;
-    idxVal.pop();
-    auto pp = idxVal.top();
-    idx = pp.first + 1;
-    curSum -= pp.second;
-  }
-  return false;  
-
-}
-
-bool findArrWithTargetSum(vector<int>& av, int curSum, int targetSum, int targetSumA, int targetSumB) {
-  int stackSum = 0, idx = 0;
-  stack<pair<int, int>> idxVal;
   
   while (idx < av.size()) {
     while (curSum > targetSum) {
       curSum -= 2 * av[idx];
       av[idx] *= -1;
-      idxVal.push(make_pair(idx, av[idx]));
+      idxVal.push_back(idx);
       idx++;
     }
-    if (curSum == targetSum && findArrWith2TargetSum(av, targetSumA, targetSumB, targetSum)) return true;
+    if (curSum == targetSum) return true;
 
-    auto p = idxVal.top();
-    curSum -= p.second;
-    av[p.first] *= -1;
-    idxVal.pop();
+    auto p = idxVal[idxVal.size()-1];
+    curSum -= 2 * av[p];
+    av[p] *= -1;
+    idxVal.pop_back();
 
     if(idxVal.empty()) break;
-    auto pp = idxVal.top();
-    curSum -= pp.second;
-    av[pp.first] *= -1;
-    idxVal.pop();
-    idx = pp.first + 1;
+    auto pp = idxVal[idxVal.size()-1];
+    curSum -= 2 * av[pp];
+    av[pp] *= -1;
+    idxVal.pop_back();
+    idx = pp + 1;
+    while (idx<av.size() && av[idx-1] == av[idx]) idx++;
   }
   return false;
 }
@@ -61,16 +41,22 @@ bool findArrWithTargetSum(vector<int>& av, int curSum, int targetSum, int target
 int main() {
   int n, x, y, ai;
   cin >> n >> x >> y >> ai;
-  int diffx = (x - ai), diffy = y, difftotal = diffx+diffy;
-  vector<int> av(n-1,0);
-  sort(av.begin(),av.end());
+  int diffx = (x - ai), diffy = y;
+  vector<int> ax(0,0), ay(0,0);
   for (int i=0; i<n-1;i++) {
-    int ax;
-    cin >> ax;
-    av[i] = ax;
+    cin >> ai;
+    if (i & 1) {
+      ax.push_back(ai);
+    } else {
+      // ループ初回はA2でy方向
+      ay.push_back(ai);
+    }
   }
-  int curSum = accumulate(av.begin(), av.end(), 0);
-  if (findArrWithTargetSum(av, curSum, difftotal, diffx, diffy)) cout << "Yes" << endl;
+  sort(ax.begin(),ax.end());
+  sort(ay.begin(),ay.end());
+  int curSumX = accumulate(ax.begin(), ax.end(), 0);
+  int curSumY = accumulate(ay.begin(), ay.end(), 0);
+  if (findArrWithTargetSum(ax,curSumX,diffx) && findArrWithTargetSum(ay,curSumY,diffy)) cout << "Yes" << endl;
   else cout << "No" << endl;
   
 }
