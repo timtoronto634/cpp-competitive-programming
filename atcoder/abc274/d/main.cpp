f00d772ba5d932194e19aec390fbbf14825dda21
timtoronto634 <tim225x@gmail.com>
2022-10-24 23:31:28 +0000

    abc274 d

--- atcoder/abc274/d/main.cpp
+++ atcoder/abc274/d/main.cpp
@@ -2,61 +2,24 @@
 using namespace std;
 using ll = long long int;
 
-bool findArrWithTargetSum(vector<int>& av, int curSum, int targetSum) {
-  if (curSum < targetSum) return false;
-  if (curSum == targetSum) return true;
-  int idx = 0;
-  vector<int> idxVal;
-  // vector<int> sidx(0);
-  // vector<int> sval(0);
-
-  
-  while (idx < av.size()) {
-    while (curSum > targetSum) {
-      curSum -= 2 * av[idx];
-      av[idx] *= -1;
-      idxVal.push_back(idx);
-      idx++;
-    }
-    if (curSum == targetSum) return true;
-
-    auto p = idxVal[idxVal.size()-1];
-    curSum -= 2 * av[p];
-    av[p] *= -1;
-    idxVal.pop_back();
-
-    if(idxVal.empty()) break;
-    auto pp = idxVal[idxVal.size()-1];
-    curSum -= 2 * av[pp];
-    av[pp] *= -1;
-    idxVal.pop_back();
-    idx = pp + 1;
-    while (idx<av.size() && av[idx-1] == av[idx]) idx++;
-  }
-  return false;
-}
-
-
-
 int main() {
-  int n, x, y, ai;
-  cin >> n >> x >> y >> ai;
-  int diffx = (x - ai), diffy = y;
-  vector<int> ax(0,0), ay(0,0);
-  for (int i=0; i<n-1;i++) {
-    cin >> ai;
-    if (i & 1) {
-      ax.push_back(ai);
+  int n,x,y;
+  cin >> n >> x >> y;
+  vector<int> A(n);
+  for (int i=0;i<n;i++) cin >> A[i];
+  bitset<20001> dp1,dp2;
+  dp1.set(10000 + A[0]);
+  dp2.set(10000);
+  for (int i=1;i<n;i++) {
+    if (i%2 == 1) {
+      dp2 = (dp2 << A[i]) | (dp2 >> A[i]);
     } else {
-      // ループ初回はA2でy方向
-      ay.push_back(ai);
+      dp1 = (dp1 << A[i]) | (dp1 >> A[i]);
     }
   }
-  sort(ax.begin(),ax.end());
-  sort(ay.begin(),ay.end());
-  int curSumX = accumulate(ax.begin(), ax.end(), 0);
-  int curSumY = accumulate(ay.begin(), ay.end(), 0);
-  if (findArrWithTargetSum(ax,curSumX,diffx) && findArrWithTargetSum(ay,curSumY,diffy)) cout << "Yes" << endl;
-  else cout << "No" << endl;
-  
+  if (dp1[10000+x] == 1 && dp2[10000+y] == 1) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
 }
