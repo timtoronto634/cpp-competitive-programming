860e3d8f8bd8ad67b305683cec20670a65a806ff
timtoronto634 <tim225x@gmail.com>
2022-11-09 23:26:42 +0000

    Fix conflicts

--- atcoder/abc276/f/main.cpp
+++ atcoder/abc276/f/main.cpp
@@ -2,6 +2,50 @@
 using namespace std;
 using ll = long long int;
 
+const ll MOD=998244353;
+long long modPow(long long x, long long a) {
+  if (a == 1) return x;
+  if (a % 2) return (x * modPow(x, a - 1)) % MOD;
+  long long t = modPow(x, a / 2);
+  return (t * t) % MOD;
+}
+
+long long modInv(ll x) {
+  return modPow(x, MOD - 2);
+}
+
 int main() {
-    return 0;
+  int n;
+  cin >> n;
+  vector<ll> cards(n);
+  for (int i=0;i<n;i++) {
+    cin >> cards[i];
+  }
+  ll curMax = cards[0];
+  // k : num of cards
+  map<int,int> counts;
+  for (int k=1;k<=n;k++) {
+    counts[cards[k-1]]++;
+    ll out = 0;
+    // double out = 0.;
+    ll p = modInv(pow(k,2));
+    // double p = 1. / pow(k,2);
+    // cout << "p for " << k << " is: " << p << endl;
+    for (int c=0;c<k;c=c) {
+      ll num;
+      for (auto el : counts) {
+        num = el.first;
+        c += el.second;
+        ll times = el.second;
+        // c: idx of card
+        ll numOcc = (2 * c + 1)%MOD;
+        // ll numOcc = (2 * c + 1);
+        out += ((num * numOcc %MOD) * p %MOD )* times % MOD;
+        // out += (cards[c] * numOcc) * p ;
+      }
+      // out %= MOD;
+    }
+    cout << out << endl;
+  }
+  return 0;
 }
--- atcoder/typical90/001/main.cpp
+++ atcoder/typical90/001/main.cpp
@@ -3,5 +3,44 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  int n,l;
+  cin >> n >> l;
+  int k;
+  cin >> k;
+  vector<int> divs(n);
+  int left = l, right = l;
+  for(int i=0;i<n;i++) {
+    cin >> divs[i];
+    left = min(left, divs[i]);
+  }
+
+  // is the answer over x?
+  int mid;
+  while (left < right) {
+    mid = left + (right - left) / 2;
+    // whether x can be 
+    bool isMidValid = true;
+
+    ll curSum = 0;
+    int cnt = 0;
+    for (int i=0;i<n;i++) {
+      curSum += divs[i];
+      if (curSum > mid) {
+        curSum = 0;
+        cnt++;
+      }
+      if (cnt > k) {
+        isMidValid = false;
+        break;
+      }
+    }
+
+    if (isMidValid) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  cout << right << endl;
   return 0;
 }
