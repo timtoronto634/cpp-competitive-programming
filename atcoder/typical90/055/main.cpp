5c4e4b9ac1e40590bbabb9c2dd001f02731c110a
timtoronto634 <tim225x@gmail.com>
2022-11-19 13:11:17 +0000

    Add 55

--- atcoder/typical90/055/main.cpp
+++ atcoder/typical90/055/main.cpp
@@ -3,5 +3,28 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  int n, p, q;
+  cin >> n >> p >> q;
+  vector<ll> v(n);
+  int ai;
+  for (int i=0;i<n;i++) {
+    cin >> ai;
+    v[i] = ai % p;
+  }
+  ll cnt = 0;
+  for (int i=0;i<n;i++) {
+    for (int j=i+1;j<n;j++) {
+      for (int k=j+1;k<n;k++) {
+        for (int l=k+1;l<n;l++) {
+          for (int m=l+1;m<n;m++) {
+            if ((((v[i] * v[j] % p) * v[k] % p) * v[l] % p) * v[m] % p == q) {
+              cnt++;
+            }
+          }
+        }
+      }
+    }
+  }
+  cout << cnt << endl;
   return 0;
 }
