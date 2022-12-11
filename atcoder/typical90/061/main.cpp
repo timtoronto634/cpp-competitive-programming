7477cdad2b45c1283b6659c28b66bc1bbbb5efa1
timtoronto634 <tim225x@gmail.com>
2022-12-11 09:58:44 +0000

    Solve 061

--- atcoder/typical90/061/main.cpp
+++ atcoder/typical90/061/main.cpp
@@ -3,5 +3,24 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  int q, t, x;
+  cin >> q;
+  vector<int> two, one;
+  for (int i=0;i<q;i++) {
+    cin >> t >> x;
+    if (t == 3) {
+      int out;
+      if (x <= one.size()) {
+        out = one[one.size()-x];
+      } else {
+        out = two[x-one.size()-1];
+      }
+      cout << out << endl;
+    } else if (t == 1) {
+      one.push_back(x);
+    } else {
+      two.push_back(x);
+    }
+  }
   return 0;
 }
