333376f2a197a2578367b7a330a46219aa167475
timtoronto634 <tim225x@gmail.com>
2022-12-11 23:03:46 +0000

    Solve 11

--- atcoder/typical90/011/main.cpp
+++ atcoder/typical90/011/main.cpp
@@ -1,40 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;
+ll dp[5009][5009];
+ll d[5009], c[5009], s[5009];
 
 int main() {
   ll n, di, ci;
   ll si;
   cin >> n;
-  vector<ll> d(n), c(n);
-  vector<ll> s(n);
+
   vector<tuple<ll, ll, ll>> end_cont_rew;
   for (ll i=0;i<n;i++) {
     cin >> di >> ci >> si;
     end_cont_rew.push_back(make_tuple(di, ci, si));
   }
-  sort(end_cont_rew.begin(), end_cont_rew.end(), [](auto const &a, auto const &b){
-    return get<0>(a) < get<0>(b);
-  });
-  ll max_reward = 0;
-  for (ll bs=1;bs<(1<<n);bs++) {
-    ll cur_day = 0; ll cur_reward = 0;
-    bool valid = true;
-
-    for (ll work=0;work<n;work++) {
-      // その仕事をするか
-      if ((bs & (1<<work)) == 0) continue;
-      // 締め切りに間に合っているか
-      if ((cur_day + get<1>(end_cont_rew[work])) > get<0>(end_cont_rew[work])) {
-        valid = false;
-        break;
-      }
-      cur_reward += get<2>(end_cont_rew[work]);
-      cur_day += get<1>(end_cont_rew[work]);
-    }
-    if (valid) max_reward = max(max_reward, cur_reward);
+  sort(end_cont_rew.begin(), end_cont_rew.end());
+  for (int i=1;i<=n;i++) {
+    d[i] = get<0>(end_cont_rew[i-1]);
+    c[i] = get<1>(end_cont_rew[i-1]);
+    s[i] = get<2>(end_cont_rew[i-1]);
   }
-  cout << max_reward << endl;
+
+  for (int i=0;i<n;i++) {
+    for (int day=0;day<5009;day++) {
+      // when not do the work i+1
+      dp[i+1][day] = max(dp[i+1][day], dp[i][day]);
+      // when do the work i+1
+      if (day + c[i+1] <= d[i+1]) {
+        dp[i+1][day+c[i+1]] = max(dp[i+1][day+c[i+1]], dp[i][day]+s[i+1]);
+      }
+    }
+  }
+  long long Answer = 0;
+	for (int i = 0; i <= 5000; i++) Answer = max(Answer, dp[n][i]);
+	cout << Answer << endl;
 
   return 0;
 }
