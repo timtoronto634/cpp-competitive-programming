a364634c15c9798966517ecb15e122ef6726d7fd
timtoronto634 <tim225x@gmail.com>
2022-12-26 22:15:09 +0000

    Solved typ 14

--- atcoder/typical90/014/main.cpp
+++ atcoder/typical90/014/main.cpp
@@ -3,5 +3,16 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  int n;
+  cin >> n;
+  vector<ll> a(n),b(n);
+  for (int i=0;i<n;i++) cin >> a[i];
+  for (int i=0;i<n;i++) cin >> b[i];
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+  ll sum = 0;
+  for (int i=0;i<n;i++) sum+=abs(a[i]-b[i]);
+  cout << sum << endl;
+
   return 0;
 }
