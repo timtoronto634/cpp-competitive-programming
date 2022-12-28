732612f98e946d70f926ac6970629b59515b9d6b
timtoronto634 <tim225x@gmail.com>
2022-12-28 00:59:48 +0000

    Failed in solving 017

--- atcoder/typical90/017/main.cpp
+++ atcoder/typical90/017/main.cpp
@@ -12,53 +12,40 @@ int main() {
   // [[1,4],[0,3],[0,2]]
   vector<line> lines(m);
   int from, to;
-  int adj[n][n];
-  for (int i=0;i<n;i++) for (int j=0;j<n;j++) adj[i][j]=0;
+  // int adj[n][n];
+  // for (int i=0;i<n;i++) for (int j=0;j<n;j++) adj[i][j]=0;
+  map<int, vector<int>> adjmap;
   for (int i=0;i<m;i++) {
     cin >> from>> to;
     from--;
     to--;
     lines[i] = line{from,to};
-    adj[from][to]++;
-    adj[to][from]++;
+    adjmap[from].push_back(to);
+    adjmap[to].push_back(from);
   }
 
-  // for each point, precalc the cumulative number of lines from the start point to each point
-  // 0: [0, 0, 1, 2, 2, 2]
-  // 1: [0, 0, 0, 0, 1, 1]
-  // 2: [1, 1, 1, 1, 1, 1]
-  // 3: [1, 1, 1, 1, 1, 1]
-  // 4: [0, 1, 1, 1, 1, 1]
-  // 5: [0, 0, 0, 0, 0, 0]
-  // map<int,int> distances;
-  int cumul[n][n];
-  for (int i=0;i<n;i++) for (int j=0;j<n;j++) cumul[i][j] = j == 0 ? adj[i][j] : cumul[i][j-1]+adj[i][j];
+  // sort each vec
+  for (auto &each : adjmap) {
+    auto &eachv = each.second;
+    sort(eachv.begin(), eachv.end());
+  }
 
-  // for each line, loop till the other end while counting how many lines crosses
-  // for [1,4]
-  // count line start from point 2 (count (4+1) to (1-1))
-  //   2[5] - 2[4] = 1 - 1 = 0
-  //   2[1-1] = 1
-  //   total 1
-  // count line start from point 3 (count (4+1) to (1-1))
-  //   3[4+1] - 3[4] = 1 - 1 = 0
-  //   3[1-1] = 1
-  //   total 1
-  // for [1,4]  total 2
-  // for [0,3]
-  // count line start from point 1 (count (3+1) to (0-1=5)) 1[5]-1[3] = 1
-  // count line start from point 2 (count (3+1) to (0-1=5)) 2[5]-2[3] = 0
-  // total 1
-  // for [0,2]
-  // count line start from point 1 (count (2+1) to (0-1=5)) 1[5]-1[2] = 1
-  // total 1
-  // total 4 overall
   ll total =0;
   for (line eachl : lines) {
     for (int startp = eachl.from+1;startp<eachl.to;startp++) {
-      total += cumul[startp][n-1] - cumul[startp][eachl.to];
-      if (eachl.from != 0) {
-        total += cumul[startp][eachl.from-1];
+      if (adjmap.count(startp) == 0) continue;
+      vector<int> &adj = adjmap[startp];
+      auto index = lower_bound(adj.begin(), adj.end(), eachl.to);
+      if (index != adj.end()) {
+        total += distance(index, adj.end());
+      }
+      
+      auto indexS = lower_bound(adj.begin(), adj.end(), eachl.from);
+      if (indexS == adj.end()) {
+        total += adj.size();
+      } else {
+
+        total += distance(adj.begin(), indexS);
       }
     }
   }
