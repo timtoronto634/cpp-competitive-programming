4a10e5bde4e9fb88f96494e0c17c53602e2b939e
timtoronto634 <tim225x@gmail.com>
2022-12-29 11:16:54 +0000

    Solve 017 second stage

--- atcoder/typical90/017/main.cpp
+++ atcoder/typical90/017/main.cpp
@@ -2,35 +2,83 @@
 using namespace std;
 using ll = long long int;
 
-struct line {int from; int to;};
+struct line {ll from; ll to;};
 
-bool crossed(line s, line t) {
-  if (s.from < t.from && t.from < s.to && s.to < t.to) return true;
-  if (t.from < s.from && s.from < t.to && t.to < s.to) return true;
-  return false;
-}
+
+long long N, M;
 
 int main() {
-  int n,m;
-  cin >> n >> m;
+  cin >> N >> M;
 
-  vector<line> lines(m);
+
+  vector<line> lines(M);
   int from, to;
-  for (int i=0;i<m;i++) {
+  for (int i=0;i<M;i++) {
     cin >> from>> to;
     from--;
     to--;
     lines[i] = line{from,to};
   }
 
-  ll total =0;
-  for (int i=0;i<m;i++) {
-    for (int j=i+1;j<m;j++) {
-      if (crossed(lines[i], lines[j])) total++;
-    }
+  // 1: same point
+  ll numSamePoint = 0;
+  ll pointCount[N];
+  for (int i=0;i<N;i++) pointCount[i] = 0;
+  for (int i=0;i<M;i++) {
+    pointCount[lines[i].from]++;
+    pointCount[lines[i].to]++;
   }
-  cout << total << endl;
-  return 0;
+  ll one = 1;
+  ll two = 2;
+  for (int i=0;i<N;i++) {
+    numSamePoint += pointCount[i] * (pointCount[i]-one) / two;
+  }
+
+  // 2: independent
+  ll cumulSum[N];
+  ll toCount[N];
+  for (int i=0;i<N;i++) cumulSum[i]=0;
+  for (int i=0;i<N;i++) toCount[i]=0;
+  for (int i=0;i<M;i++) {
+    toCount[lines[i].to]++;
+  }
+  cumulSum[0] = toCount[0];
+  for (int i=1;i<N;i++) {
+    cumulSum[i] = cumulSum[i-1] + toCount[i];
+  }
+  // calculate
+  ll numIndependent = 0;
+  for (int i=0;i<M;i++) {
+    if (lines[i].from == 0) continue;
+    numIndependent += cumulSum[lines[i].from -1];
+  }
+
+  // 3: inclusive
+
+  // THIS IS WRONG WAY TO SORT
+  // sort(lines.begin(), lines.end(), [](auto const& lhs, auto const& rhs) {
+  //   if (lhs.to != rhs.to) return lhs.to < rhs.to;
+  //   else return lhs.from < rhs.from;
+  // });
+
+	vector<pair<long long, long long>> vec;
+	for (int i = 0; i < M; i++) vec.push_back(make_pair(lines[i].to, lines[i].from));
+	sort(vec.begin(), vec.end());
+  ll numFrom[N];
+  for (int i=0;i<N;i++) numFrom[i]=0;
+  ll numInclusive = 0;
+  for (int i=0;i<M;i++) {
+    auto eachl = vec[i];
+    for (int cntPoint=eachl.second+1; cntPoint <= eachl.first-1; cntPoint++) {
+      numInclusive += numFrom[cntPoint];
+    }
+    numFrom[eachl.second]++;
+  }
+
+	long long Total = M * (M - 1) / 2LL;
+	long long SumAns = numSamePoint + numIndependent + numInclusive;
+	cout << Total - SumAns << endl;
+	return 0;
 }
 
 
