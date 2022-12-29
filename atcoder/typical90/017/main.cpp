11feaf30186b9df14d4ac780241eeb778a4df293
timtoronto634 <tim225x@gmail.com>
2022-12-29 09:26:02 +0000

    solve 017 with  brute force

--- atcoder/typical90/017/main.cpp
+++ atcoder/typical90/017/main.cpp
@@ -4,55 +4,32 @@ using ll = long long int;
 
 struct line {int from; int to;};
 
+bool crossed(line s, line t) {
+  if (s.from < t.from && t.from < s.to && s.to < t.to) return true;
+  if (t.from < s.from && s.from < t.to && t.to < s.to) return true;
+  return false;
+}
+
 int main() {
   int n,m;
   cin >> n >> m;
 
-  // store input
-  // [[1,4],[0,3],[0,2]]
   vector<line> lines(m);
   int from, to;
-  // int adj[n][n];
-  // for (int i=0;i<n;i++) for (int j=0;j<n;j++) adj[i][j]=0;
-  map<int, vector<int>> adjmap;
   for (int i=0;i<m;i++) {
     cin >> from>> to;
     from--;
     to--;
     lines[i] = line{from,to};
-    adjmap[from].push_back(to);
-    adjmap[to].push_back(from);
-  }
-
-  // sort each vec
-  for (auto &each : adjmap) {
-    auto &eachv = each.second;
-    sort(eachv.begin(), eachv.end());
   }
 
   ll total =0;
-  for (line eachl : lines) {
-    for (int startp = eachl.from+1;startp<eachl.to;startp++) {
-      if (adjmap.count(startp) == 0) continue;
-      vector<int> &adj = adjmap[startp];
-      auto index = lower_bound(adj.begin(), adj.end(), eachl.to);
-      if (index != adj.end()) {
-        total += distance(index, adj.end());
-      }
-      
-      auto indexS = lower_bound(adj.begin(), adj.end(), eachl.from);
-      if (indexS == adj.end()) {
-        total += adj.size();
-      } else {
-
-        total += distance(adj.begin(), indexS);
-      }
+  for (int i=0;i<m;i++) {
+    for (int j=i+1;j<m;j++) {
+      if (crossed(lines[i], lines[j])) total++;
     }
   }
-
-  // halves at the end
-  // return 2
-  cout << total/2 << endl;
+  cout << total << endl;
   return 0;
 }
 
