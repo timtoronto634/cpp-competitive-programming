a4ae950e971427e7a7c72ad47238b498db3d9b5f
timtoronto634 <tim225x@gmail.com>
2022-12-31 03:32:43 +0000

    Solved 020

--- atcoder/typical90/020/main.cpp
+++ atcoder/typical90/020/main.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 using ll = long long int;
 
+ll a, b, c;
+
+ll numPow(ll c, ll b) {
+  if (b==1) return c;
+  ll half = numPow(c, (b/2));
+  if (b & 1 == 1) return half * half * c;
+  else return half * half;
+}
+
 int main() {
+  cin >> a >> b >> c;
+  cout << ((a < numPow(c,b)) ? "Yes" : "No") << endl;
   return 0;
 }
