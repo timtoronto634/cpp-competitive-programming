9ec17633c299de2b7c973db41a2196a11f9abe84
timtoronto634 <tim225x@gmail.com>
2023-01-01 10:00:39 +0000

    Remove comments

--- my-library/scc.cpp
+++ my-library/scc.cpp
@@ -43,25 +43,10 @@ vector<vector<int>> tarjan(int n, map<int,vector<int>> &edges) {
   for (int node=1;node<=n;node++) {
     if (visited.count(node) == 1) continue;
     level = dfs(node, level, edges, visited);
-    // cout << "node " << node << " done. level: " << level << endl;
-    
   }
-  // 
-  //   for (auto each_edge : edges) {
-  //   cout << each_edge.first << ": ";
-  //   for (auto each_to : each_edge.second) {
-  //     cout << each_to << " ";
-  //   }
-  //   cout << endl;
-  // }
+
   map<int,vector<int>> reversed = reverseEdges(edges);
-  // for (auto each_edge : reversed) {
-  //   cout << each_edge.first << ": ";
-  //   for (auto each_to : each_edge.second) {
-  //     cout << each_to << " ";
-  //   }
-  //   cout << endl;
-  // }
+
   set<int> visited2;
   vector<vector<int>> sccs;
   for (int node_level=n;node_level>=1;node_level--) {
@@ -85,6 +70,16 @@ void print(vector<vector<int>> v) {
   }
 }
 
+void printEdge(map<int,vector<int>> edges) {
+  for (auto each_edge : edges) {
+    cout << each_edge.first << ": ";
+    for (auto each_to : each_edge.second) {
+      cout << each_to << " ";
+    }
+    cout << endl;
+  }
+}
+
 void check(vector<vector<int>> expected, vector<vector<int>> actual) {
   sort(expected.begin(), expected.end());
   sort(actual.begin(), actual.end());
@@ -99,13 +94,25 @@ void check(vector<vector<int>> expected, vector<vector<int>> actual) {
       }
     }
   }
-  // cout << "correct" << endl;
-
 }
 
 int main() {
-  map<int, vector<int>> mp{{1, vector<int>{2}}, {2, vector<int>{1,3}}};
-  // check(vector<vector<int>>{{1,2},{3}}, tarjan(3, mp));
-  print(tarjan(3,mp));
-  // cout << 0 << endl;
+  // cout << "sample 1" << endl;
+  // map<int, vector<int>> mp{{1, vector<int>{2}}, {2, vector<int>{1,3}}};
+  // print(tarjan(3,mp));
+  cout << "sample 2" << endl;
+  map<int, vector<int>> mp2{
+    {1, vector<int>{2}}, // A->B
+    {2, vector<int>{3,11}},  // B->C, K
+    {3, vector<int>{1,4}},
+    {4, vector<int>{5}},
+    {5, vector<int>{6, 11}},
+    {6, vector<int>{7}},
+    {7, vector<int>{4}},
+    {8, vector<int>{9,10}}, // H->I,J
+    {9, vector<int>{11}},
+    {10, vector<int>{8}},
+    {11, vector<int>{8}},
+  };
+  print(tarjan(11, mp2));
 }
