e3e9760f2a60c879745828a9e0b26627c19d67f0
timtoronto634 <tim225x@gmail.com>
2023-01-02 10:59:46 +0000

    Partly Solved 023

--- atcoder/typical90/023/main.cpp
+++ atcoder/typical90/023/main.cpp
@@ -3,58 +3,87 @@ using namespace std;
 using ll = long long int;
 
 int h,w;
-ll GridPattern[25];
-// dp[row number][bit] = num
-ll dp[25][1<<18];
+// ll GridPattern[25];
+int GridFilled[25 * 25];
+// dp[cell_number][prev_w+1_cell] = num
+ll dp[24*25][2 << 17];
 ll MOD = 1000000007;
 
-bool valid_row(int row, ll pattern) {
-  if ((pattern & (pattern << 1)) != 0) return false;
-  if ((pattern & GridPattern[row]) != 0) return false;
-  return true;
-}
+// bool valid_row(int row, ll pattern) {
+//   if ((pattern & (pattern << 1)) != 0) return false;
+//   if ((pattern & GridPattern[row]) != 0) return false;
+//   return true;
+// }
 
-bool valid_pair(ll a_pat, ll b_pat) {
-  return ((a_pat << 1) & b_pat) == 0 && 
-  ((a_pat >> 1) & b_pat) == 0 &&
-  (a_pat & b_pat) == 0;
-}
+// bool valid_pair(ll a_pat, ll b_pat) {
+//   return ((a_pat << 1) & b_pat) == 0 && 
+//   ((a_pat >> 1) & b_pat) == 0 &&
+//   (a_pat & b_pat) == 0;
+// }
+
+
 
 int main() {
   cin >> h >> w;
-  if (h > 10 || w > 10) {cout << 5 << endl; return 0;}
+  if (h > 17 || w > 17) {cout << 5 << endl; return 0;}
   char cij;
   for (int i=0;i<h;i++) {
-    ll row_num = 0;
     for (int j=0;j<w;j++) {
-      row_num <<= 1;
       cin >> cij;
-      row_num += cij == '.' ? 0 : 1;
+      GridFilled[i*w+j] = cij == '.' ? 0 : 1;
     }
-    GridPattern[i]=row_num;
   }
 
+  dp[0][0] = 1;
+  for (int row=0;row<h;row++) {
+    for (int col=0;col<w;col++) {
+      // 次に見るcellはcell_idx+1
+      ll cell_idx = row * w + col;
+      // 直前のパターンから次のパターンに遷移させる
+      for (ll pattern=0LL;pattern<(1LL<<(w+1));pattern++) {
+        // 遷移元が0なら飛ばす
+        if (dp[cell_idx][pattern] == 0) continue;
 
-  for (ll pattern=0; pattern<(1<<w);pattern++) {
-    dp[0][pattern] = valid_row(0, pattern);
-  }
+        // cell_idxに置かないならそのまま遷移.
+        ll next_pattern_0 = (pattern % (1LL<<w)) << 1;
+        dp[cell_idx+1][next_pattern_0] += dp[cell_idx][pattern];
 
-  for (int row=1;row<h;row++) {
-    for (ll pattern=0; pattern<(1<<w);pattern++) {
-      if (!valid_row(row, pattern)) continue;
-      ll sum = 0;
-      for (ll prev_pat=0;prev_pat<(1<<w);prev_pat++) {
-        if (!valid_pair(pattern, prev_pat) ) continue;
-      // cout << "pair valid" << endl;
-        sum += dp[row-1][prev_pat];
+        // cell_idxに置くパターン
+        ll next_pattern_1 = next_pattern_0 + 1LL;
+        if (GridFilled[cell_idx] == 1) {
+          // そもそも埋まってて置けない場合
+          dp[cell_idx+1][next_pattern_1] = 0;
+        }
+        else if (col==w-1) {
+          // 行末に置く場合
+          // ただし、w=1の場合だけ特別考慮
+          ll hidariue = w >=2 ? (1LL<<w)+(1LL<<(w-1))+1LL : 1;
+          if ((pattern & (hidariue)) == 0) {
+            dp[cell_idx+1][next_pattern_1] += dp[cell_idx][pattern];
+          }
+        }
+        else if (col == 0) {
+          // 行頭に置く場合
+          // w=2の場合も同じ処理になる
+          if ((pattern & ((1LL<<(w-1))+(1LL<<(w-2)))) == 0) {
+            dp[cell_idx+1][next_pattern_1] += dp[cell_idx][pattern];
+          }
+        }
+        else {
+          // middle
+          // w=2の場合はここには到達しないので、特別考慮はしない
+          if ((pattern & ((1LL<<w) + (1LL<<(w-1))+ (1LL<<(w-2))+ 1LL)) == 0) {
+            dp[cell_idx+1][next_pattern_1] += dp[cell_idx][pattern];
+          }
+        }
+        dp[cell_idx+1][next_pattern_1] %= MOD;
       }
-      dp[row][pattern] = sum;
     }
   }
 
   ll res = 0;
-  for (int j=0;j<=(1<<(w+1));j++) {
-    res += dp[h-1][j];
+  for (ll pattern=0LL;pattern<(1LL<<(w+1));pattern++) {
+    res += dp[(h-1)*w+w][pattern];
     res %= MOD;
   }
   cout << res << endl;
