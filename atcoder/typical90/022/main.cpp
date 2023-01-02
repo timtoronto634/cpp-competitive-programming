7a7ab3ca2d78841f6ae4f7e3e4ba9225c2b49edc
timtoronto634 <tim225x@gmail.com>
2023-01-02 00:33:37 +0000

    Solved 022

--- atcoder/typical90/022/main.cpp
+++ atcoder/typical90/022/main.cpp
@@ -3,5 +3,9 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  ll a,b,c;
+  cin >> a >> b >> c;
+  ll part = gcd(gcd(a,b), c) ;
+  cout << (max(a/part-1, 0LL)+max(b/part-1,0LL)+max(c/part-1,0LL)) << endl;
   return 0;
 }
