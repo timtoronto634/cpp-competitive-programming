764956aa567b2c9797813fd1aa48656992357e9a
timtoronto634 <tim225x@gmail.com>
2023-01-03 01:21:58 +0000

    Solved 024

--- atcoder/typical90/024/main.cpp
+++ atcoder/typical90/024/main.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 using ll = long long int;
 
+int A[1000], B[1000];
 int main() {
+  int n,k;
+  cin >> n >> k;
+  for (int i=0;i<n;i++) {
+    cin >> A[i];
+  }
+  for (int i=0;i<n;i++) {
+    cin >> B[i];
+  }
+  ll diff = 0;
+  for (int i=0;i<n;i++) {
+    diff += abs(A[i] - B[i]);
+  }
+  if ((k >= diff) && ((diff - k) % 2 == 0)) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+
+
   return 0;
 }
