df5a5b6b53cfda8cf06a2bf069c3a8d2fed65471
timtoronto634 <tim225x@gmail.com>
2023-01-05 13:38:43 +0000

    Solved 27, 28

--- atcoder/typical90/027/main.cpp
+++ atcoder/typical90/027/main.cpp
@@ -3,5 +3,16 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  int n;
+  string s;
+  cin >> n;
+  set<string> registered;
+  for (int i=1;i<=n;i++) {
+    cin >> s;
+    if (registered.count(s)==0) {
+      cout << i << endl;
+    }
+    registered.insert(s);
+  }
   return 0;
 }
--- atcoder/typical90/028/main.cpp
+++ atcoder/typical90/028/main.cpp
@@ -2,6 +2,41 @@
 using namespace std;
 using ll = long long int;
 
+int coord[10010][10010];
+int overwrap[100010];
+
 int main() {
+  int n, leftx, lefty, rightx, righty;
+  cin >> n;
+  for (int i=0;i<n;i++) {
+    cin >> leftx >> lefty >> rightx >> righty;
+    int lefttopx = leftx, lefttopy = righty, rightbottomx=rightx, rightbottomy=lefty;
+    coord[lefttopx][lefttopy]--;
+    coord[leftx][lefty]++;
+    coord[rightx][righty]++;
+    coord[rightbottomx][rightbottomy]--;
+  }
+
+  for (int i=0;i<10010;i++) {
+    for (int j=1;j<10010;j++) {
+      coord[i][j] += coord[i][j-1];
+    }
+  }
+  
+  for (int i=1;i<10010;i++) {
+    for (int j=0;j<10010;j++) {
+      coord[i][j] += coord[i-1][j];
+    }
+  }
+
+  for (int i=0;i<10010;i++) {
+    for (int j=0;j<10010;j++) {
+      overwrap[coord[i][j]]++;
+    }
+  }
+
+  for (int k=1;k<=n;k++) {
+    cout << overwrap[k] << endl;
+  }
   return 0;
 }
