de8498f1aa692600cbf06f5cbe87f18a23fdb82e
timtoronto634 <tim225x@gmail.com>
2023-01-08 15:37:27 +0000

    solved 30

--- atcoder/typical90/030/main.cpp
+++ atcoder/typical90/030/main.cpp
@@ -3,5 +3,19 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  ll N, K;
+  cin >> N >> K;
+  vector<int>nums(N+1,0);
+  for (int p=2;p<=N;p++) {
+    if (nums[p] != 0) continue;
+    for (int bai=p; bai<=N;bai+=p) {
+      nums[bai]++;
+    }
+  }
+  int ans=0;
+  for (int cnt=0;cnt<=N;cnt++) {
+    if (nums[cnt] >= K) ans++;
+  }
+  cout << ans << endl;
   return 0;
 }
