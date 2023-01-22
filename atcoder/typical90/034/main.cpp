e65edc208af0e183a71c85247adae58f74ec2cfd
timtoronto634 <tim225x@gmail.com>
2023-01-22 11:50:30 +0000

    Solved 34

--- atcoder/typical90/034/main.cpp
+++ atcoder/typical90/034/main.cpp
@@ -3,5 +3,39 @@ using namespace std;
 using ll = long long int;
 
 int main() {
+  int n, k, ai;
+  cin >> n >> k;
+  vector<int> v;
+  for (int i=0;i<n;i++) {
+    cin >> ai;
+    v.push_back(ai);
+  }
+  int left=0;
+  int right=0;
+  int kind=0;
+  int maxrange = 0;
+  unordered_map<int, int> cnts;
+
+  while (left <= right && right < n) {
+    while (kind <= k && right < n) {
+      int nexnum = v[right];
+      if (kind == k && cnts[nexnum] == 0) {
+        // if nexnum is added, the kind is over k
+        break;
+      }
+      if (cnts[nexnum] == 0) kind++;
+      cnts[nexnum]++;
+      right++;
+      maxrange = max(right - left, maxrange);
+    }
+
+    while (kind == k && left <= right) {
+      int leftnum = v[left];
+      cnts[leftnum]--;
+      if (cnts[leftnum] == 0) kind--;
+      left++;
+    }
+  }
+  cout << maxrange << endl;
   return 0;
 }
